2aeb14d4bc184ffcb5ab129b4459b3f3c254183d
Ved Vyapak <vedvyapak@gmail.com>
2021-03-25 00:31:50 +0530

    Services Incomplete - Responsive

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import AboutUs from './Container/AboutUs/AboutUs';
 import Banner from './Container/Banner/Banner';
+import Services from './Container/Services/Services';
 // import Footer from './Container/Footer/Footer';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="app">
       <Banner/>
       <AboutUs/>
+      <Services/>
       {/* <Footer/> */}
     </div>
   );
