76a373914b01d769b96257adf3e1c1e210bdcae9
Ved Vyapak <vedvyapak@gmail.com>
2021-03-30 01:42:35 +0530

    Contact Us - Responsive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import AboutUs from './Container/AboutUs/AboutUs';
 import Banner from './Container/Banner/Banner';
+import ContactUs from './Container/ContactUs/ContactUs';
 import Services from './Container/Services/Services';
+
+import Suggested from './Container/Suggested/Suggested';
 // import Footer from './Container/Footer/Footer';
 
 function App() {
@@ -10,6 +13,8 @@ function App() {
       <Banner/>
       <AboutUs/>
       <Services/>
+      <ContactUs/>
+      {/* <Suggested/> */}
       {/* <Footer/> */}
     </div>
   );
